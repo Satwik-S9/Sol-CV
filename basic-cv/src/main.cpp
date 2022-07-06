d46f54d3704af367bc3ce092f4d1e5763643a67c
Satwik Srivastava <satwiksri27@gmail.com>
2022-07-06 23:39:47 +0530

    CPP Source file(s) with updated method calls

--- basic-cv/src/main.cpp
+++ basic-cv/src/main.cpp
@@ -7,19 +7,21 @@
 #include "../include/dip.h"
 #include "../include/utils.h"
 #include "../include/filters.h"
+#include "../include/imgops.h"
+#include "../include/transforms.h"
 
 const cv::String ROOT = "/home/satwik/dev/machine-learning/computer-vision/basic-cv/";
 const cv::String OUT = ROOT + "output/";
 
 int main(int argc, char** argv) {    
     // PREPARE IMG-DATA
-    cv::String path = ROOT + "images/sample2.jpg";
+    cv::String path = ROOT + "images/sample1.jpg";
 
     // PREPARE OUTPUT FILE
     std::string outpath = (std::string)OUT + "output.txt";
     FILE* outfile = fopen(const_cast<char*>(outpath.c_str()), "a");
-    std::string sep = getSeparator(69);
-    fprintf(outfile, "%s", sep.c_str());
+    // std::string sep = getSeparator(69);
+    // fprintf(outfile, "%s", sep.c_str());
     
     // LOAD IMG
     cv::Mat_<cv::Vec3b> img = cv::imread(path);
@@ -30,8 +32,9 @@ int main(int argc, char** argv) {
     }
     
     fprintf(outfile, "  ->  IMG-DIM: {%d, %d}\n", img.rows, img.cols);
+    // cv::Mat bord = shiftDiff(img, 2, 0);
 
-    // QUANTIZE
+    //* QUANTIZE
     clock_t start = clock();
     int level = 3;
     cv::Mat_<cv::Vec3b> qimg = quantize(img, level);
@@ -44,10 +47,25 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    fprintf(outfile, "  ->  CPP - Time Taken to Quantize Image to level %d: %.6fs\n",
-             level, time_spent);
+    // fprintf(outfile, "  ->  CPP - Time Taken to Quantize Image to level %d: %.6fs\n",
+    //          level, time_spent);
 
+    //* APPLY SKETCH FILTER
     cv::Mat_<cv::Vec3b> skImg = sketchFilter(img);
+
+    //* TRANSFORMS
+    // cv::Mat m1 = cv::Mat::eye(3, 3, CV_8UC1);
+    // m1.at<uchar>(0, 2) = 1;
+    // m1.at<uchar>(1, 2) = 0;
+    // print2DMat(m1, outfile, "\t");
+    // cv::Mat m2({4, 5, 1});
+    // cv::Mat r = imgDot(m1, m2);
+    // int ni = r.at<uchar>(0);
+    // int nj = r.at<uchar>(1);
+    // std:: cout << "\n\n" << ni << "   " << nj << "\n\n";
+    cv::Mat T = getTransformMatrix(5, 5, 't');
+    print2DMat(T);
+    cv::Mat res = affineTransform(img, T);
 
     // SHOW AND SAVE
     cv::String windowName1 = "Normal";
@@ -60,15 +78,17 @@ int main(int argc, char** argv) {
     cv::imshow(windowName1, img);
     cv::imshow(windowName2, qimg);
     cv::imshow(windowName3, skImg);
+    cv::imshow("transform", res);
     
     cv::waitKey(0);
     cv::destroyAllWindows();
 
-    cv::String qimg_path = OUT + "qimg-2-8.jpg";
-    cv::String skimg_path = OUT + "skImg-2-8.jpg";
-    cv::imwrite(qimg_path, qimg);
-    cv::imwrite(skimg_path, skImg);
+    // cv::String qimg_path = OUT + "qimg-2-8.jpg";
+    // cv::String skimg_path = OUT + "skImg-2-8.jpg";
+    // cv::imwrite(qimg_path, qimg);
+    // cv::imwrite(skimg_path, skImg);
 
+    // print2DMat(r, outfile, "\t");
     fclose(outfile);
 
     return 0;
